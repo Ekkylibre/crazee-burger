bc22855accc99dead0db8c7e2c7b49d776c25c89
Dany XIONG <dany.xiong.dev@outlook.fr>
2023-12-13 14:34:42 +0100

    ✨ F01: LOGIN > Create LoginPage

---
+++ src/components/pages/LoginPages.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+export default function LoginPages() {
+   // state
+   const [inputValue,setinputValue] = useState("")
+
+   // comportement
+   const handleSubmit = (event) => { 
+     event.preventDefault()
+     alert(`Bonjour ${inputValue}`)
+    }
+ 
+    const handleChange = (event) => { 
+     const newInputValue = event.target.value
+     setinputValue(newInputValue)
+     }
+ 
+   // affichage
+   return <div>
+     <h1>Bienvenue chez nous !</h1>
+     <br/>
+     <h2>Connectez-vous</h2>
+     <form action="submit" onSubmit={handleSubmit}>
+       <input value={inputValue} onChange={handleChange} type="text" placeholder="Entre votre prénom..." required/>
+       <button>Accédez à votre espace</button>
+     </form>
+   </div>
+}
