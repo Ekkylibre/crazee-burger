688fc418255a50de822e7b28d136245ef3051b40
Dany XIONG <dany.xiong.dev@outlook.fr>
2023-12-13 14:01:26 +0100

    🎉  App.js > Add my firstname

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Salut Dany</h1>
+      <h1>Salut Dan</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
