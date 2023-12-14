247c84ef4813af811af7b49388e09541d6876c6c
Dany XIONG <dany.xiong.dev@outlook.fr>
2023-12-14 16:47:08 +0100

    ✨ F02: LOGIN > add redirection from/to OrderPage

--- src/components/pages/order/OrderPages.jsx
+++ src/components/pages/order/OrderPages.jsx
@@ -1,3 +1,10 @@
+import { Link } from "react-router-dom";
+
 export default function OrderPages() {
-  return <div>OrderPages</div>
+  return <div>
+    <h1>Bonjour</h1>
+    <Link to="/F02"
+    ><button>Déconnexion</button>
+    </Link>
+  </div>
 }
