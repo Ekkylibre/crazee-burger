5ef31404057e116d2caf8619e9208b924466b074
Dany XIONG <dany.xiong.dev@outlook.fr>
2024-07-13 14:54:43 +0200

    💄 F05: ORDER > LENU > Add final style

--- src/components/pages/order/Navbar/Navbar.jsx
+++ src/components/pages/order/Navbar/Navbar.jsx
@@ -5,26 +5,27 @@ import { theme } from "../../../../theme";
 import { refreshPage } from "../../../../utils/window";
 
 export default function Navbar({ username }) {
-  
   return (
     <NavbarStyled>
-      <Logo className="logo-order-page" onClick={() =>refreshPage()}/>
+      <Logo className="logo-order-page" onClick={() => refreshPage()} />
       <NavBarRightSide username={username} />
     </NavbarStyled>
-  )
+  );
 }
 
-
 const NavbarStyled = styled.nav`
-    background: ${theme.colors.white};
-    height: 10vh;
-    display: flex;
-    justify-content: space-between;
-    padding: 0 20px;
-    border-top-left-radius: ${theme.borderRadius.extraRound};
-    border-top-right-radius: ${theme.borderRadius.extraRound};
-    .logo-order-page {
-      cursor: pointer;
-    }
-    /* align-items: center; */
+  background: ${theme.colors.white};
+  height: 10vh;
+  display: flex;
+  justify-content: space-between;
+  padding: 0 20px;
+  border-top-left-radius: ${theme.borderRadius.extraRound};
+  border-top-right-radius: ${theme.borderRadius.extraRound};
+  border-bottom: 10px solid ${theme.colors.greyLight};
+
+  .logo-order-page {
+    cursor: pointer;
+  }
+
+  /* align-items: center; */
 `;
