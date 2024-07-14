dfd47884f4ed7acb40edab0870d17ff2f2d25bf0
Dany XIONG <dany.xiong.dev@outlook.fr>
2024-07-14 15:28:50 +0200

    💄 F05: ORDER > Product > Add image layout

--- src/components/pages/order/Main/Product.jsx
+++ src/components/pages/order/Main/Product.jsx
@@ -7,8 +7,8 @@ export default function Product({ title, price, imageSource }) {
                 <img src={imageSource} alt={title} />
             </div>
             <div className="info-text">
+                <div className="title">{title}</div>
                 <div className="description">
-                    <div className="title">{title}</div>
                     <div className="price">{price}€</div>
                     <button className="add-button">Ajouter</button>
                 </div>
@@ -18,24 +18,28 @@ export default function Product({ title, price, imageSource }) {
 }
 
 const ProductStyled = styled.div`
-
-        background: red;
-        width: 240px;
-        height: 330px;
-
-
+    background: red;
+    width: 200px;
+    height: 300px;
+    display: grid;
+    grid-template-rows: 65% 1fr;
+    padding: 20px;
+    padding-bottom: 10px;
+        
         .image {
-          width: 100px;
-          height: auto;
-          img {
+            border: 1px solid yellow;
             width: 100%;
-            height: 100%;
-            object-fit: cover;
-          }
+            height: auto;
+            margin-top: 30px;
+
+            img {
+                width: 100%;
+                height: 100%;
+                object-fit: contain;
+            }
         }
 
-        .description {
-          border: 1px solid fuchsia;
+        .info-text {
+          border: 1px solid blue;
         }
-
 `;
