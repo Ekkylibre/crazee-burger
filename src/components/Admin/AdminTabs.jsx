afc5125e85237adec08bede4905d94aa456a305a
Dany XIONG <dany.xiong.dev@outlook.fr>
2024-11-21 18:12:34 +0100

    💄 F07: ADMIN > Tabs > Add click and active style

--- src/components/Admin/AdminTabs.jsx
+++ src/components/Admin/AdminTabs.jsx
@@ -1,15 +1,23 @@
 import styled from "styled-components";
 import Tab from "../reusable-ui/Tab";
-import {FiChevronDown, FiChevronUp} from "react-icons/fi";
+import { FiChevronDown, FiChevronUp } from "react-icons/fi";
+import { theme } from "../../theme";
 
-export default function AdminTabs() {
-  return (
-    <AdminTabsStyled>
-        <Tab Icon = {<FiChevronDown />}/>
-    </AdminTabsStyled>
-  )
+export default function AdminTabs({ isCollapsed, setisCollapsed }) {
+
+    return (
+        <AdminTabsStyled>
+            <Tab Icon={isCollapsed ? <FiChevronUp /> : <FiChevronDown/>} onClick={() => setisCollapsed(!isCollapsed)} 
+            className={isCollapsed ? "is-active": ""}/>
+        </AdminTabsStyled>
+    )
 }
 
 const AdminTabsStyled = styled.div`
   /* border: 1px solid red; */
+
+  .is-active {
+    background: ${theme.colors.background_dark};
+    color: ${theme.colors.white}
+  }
 `;
