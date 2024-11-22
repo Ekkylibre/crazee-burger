47fd4355b650f6d8ce9590830389fbc508006d3b
Dany XIONG <dany.xiong.dev@outlook.fr>
2024-11-22 11:09:34 +0100

    ✨ F07: ADMIN > Tab > Add label

--- src/components/Admin/AdminTabs.jsx
+++ src/components/Admin/AdminTabs.jsx
@@ -1,23 +1,32 @@
 import styled from "styled-components";
 import Tab from "../reusable-ui/Tab";
 import { FiChevronDown, FiChevronUp } from "react-icons/fi";
+import { AiOutlinePlus } from "react-icons/ai";
 import { theme } from "../../theme";
 
 export default function AdminTabs({ isCollapsed, setisCollapsed }) {
 
-    return (
-        <AdminTabsStyled>
-            <Tab Icon={isCollapsed ? <FiChevronUp /> : <FiChevronDown/>} onClick={() => setisCollapsed(!isCollapsed)} 
-            className={isCollapsed ? "is-active": ""}/>
-        </AdminTabsStyled>
-    )
+  return (
+    <AdminTabsStyled>
+      <Tab label="" Icon={isCollapsed ? <FiChevronUp /> : <FiChevronDown />} onClick={() => setisCollapsed(!isCollapsed)}
+        className={isCollapsed ? "is-active" : ""} />
+      <Tab label="Ajouter un produit" Icon={<AiOutlinePlus />}
+        className={isCollapsed ? "is-active" : ""} />
+    </AdminTabsStyled>
+  )
 }
 
 const AdminTabsStyled = styled.div`
   /* border: 1px solid red; */
+  display: flex;
+  padding: 0 20px;
 
   .is-active {
     background: ${theme.colors.background_dark};
     color: ${theme.colors.white}
+  }
+
+  button {
+    margin-left: 1px;
   }
 `;
