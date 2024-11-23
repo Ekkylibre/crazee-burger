11d87fbb3426a70f10f5c0dfcbf0c73d02e05e4e
Dany XIONG <dany.xiong.dev@outlook.fr>
2024-11-23 16:29:44 +0100

    🚧 F08: ADMIN > Panel > Add AddForm & EditForm

--- src/components/pages/order/OrderPages.jsx
+++ src/components/pages/order/OrderPages.jsx
@@ -9,7 +9,7 @@ import { useParams } from "react-router-dom";
 export default function OrderPage() {
   // state
   const { username } = useParams()
-  const [isModeAdmin, setIsModeAdmin] = useState(false)
+  const [isModeAdmin, setIsModeAdmin] = useState(true)
   const [isCollapsed, setIsCollapsed] = useState(false)
   const [currentTabSelected, setCurrentTabSelected] = useState("add")
 
