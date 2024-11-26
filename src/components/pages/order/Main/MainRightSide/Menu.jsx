c93bc75a22ff90481f72c5c41bfb0912033f372f
Dany XIONG <dany.xiong.dev@outlook.fr>
2024-11-26 12:29:33 +0100

    💄 F08: MENU > Add EmptyMenu styles (in Admin and Client mode)

--- src/components/pages/order/Main/MainRightSide/Menu.jsx
+++ src/components/pages/order/Main/MainRightSide/Menu.jsx
@@ -4,19 +4,25 @@ import { theme } from "../../../../../theme";
 import Card from "../../../../reusable-ui/Card";
 import { formatPrice } from "../../../../../utils/maths";
 import OrderContext from "../../../../context/OrderContext";
+import EmptyMenuClient from "./Menu/EmptyMenuClient";
+import EmptyMenuAdmin from "./Menu/EmptyMenuAdmin";
 
 const IMAGE_BY_DEFAULT = "/images/coming-soon.png"
 
 export default function Menu() {
   const { menu, isModeAdmin, handleDelete, resetMenu } = useContext(OrderContext)
+  // state
 
-  if (menu.length ===0) return <div>
-    <span>Pas de produit</span>
-    <button onClick={resetMenu}>Générer à nouveau produits</button>
-  </div>
-  
+  // comportements
+
+  // affichage
+  if (menu.length === 0) {
+    if (!isModeAdmin) return <EmptyMenuClient />
+    return <EmptyMenuAdmin onReset={resetMenu} />
+  }
+
   return (
-    <MenuStyled>
+    <MenuStyled className="menu">
       {menu.map(({ id, title, imageSource, price }) => {
         return (
           <Card
@@ -25,22 +31,22 @@ export default function Menu() {
             imageSource={imageSource ? imageSource : IMAGE_BY_DEFAULT}
             leftDescription={formatPrice(price)}
             hasDeleteButton={isModeAdmin}
-            onDelete={()=>handleDelete(id)}
+            onDelete={() => handleDelete(id)}
           />
-        );
+        )
       })}
     </MenuStyled>
   )
 }
 
 const MenuStyled = styled.div`
-      background: ${theme.colors.background_white};
-      display: grid;
-      grid-template-columns: repeat(4, 1fr);
-      grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
-      grid-row-gap: 60px;
-      padding: 50px 50px 150px;
-      justify-items: center;
-      box-shadow: ${theme.shadows.strong};
-      overflow-y: scroll;
-`;
+  background: ${theme.colors.background_white};
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+  grid-row-gap: 60px;
+  padding: 50px 50px 150px;
+  justify-items: center;
+  box-shadow: 0px 8px 20px 8px rgba(0, 0, 0, 0.2) inset;
+  overflow-y: scroll;
+`
